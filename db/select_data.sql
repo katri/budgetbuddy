bdd3bd58c0a0d8f843e2437a55b97560e15ad5db
Katri <katri.pokats@gmail.com>
2022-09-12 16:37:36 +0300

    KATRI changed select_data.sql to show user ID

--- db/select_data.sql
+++ db/select_data.sql
@@ -1,14 +1,15 @@
 -- kokku liidetud standard tabelite vaade
-SELECT sc.name AS "standard gategories", ss.name AS "standard subgategories"
+SELECT sc.name AS "standard categories", ss.name AS "standard subcategories"
 FROM standard_category_relation
          JOIN standard_category sc ON sc.id = standard_category_relation.standard_category_id
          JOIN standard_subcategory ss ON ss.id = standard_category_relation.standard_subcategory_id;
 
 
 -- kokku liidetud custom kliendi tabelite vaade
-SELECT c.name AS "gategories", s.name AS "subgategories"
+SELECT c.user_id AS user, c.name AS "categories", s.name AS "subcategories"
 FROM category_relation
          JOIN category c ON c.id = category_relation.category_id
          JOIN subcategory s ON s.id = category_relation.subcategory_id;
+
 
 
